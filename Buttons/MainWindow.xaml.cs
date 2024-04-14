51f2148a2888eda9723d7678452d32cac9805db8
Elvin0802 <elvincode1517@gmail.com>
2024-04-14 19:21:02 +0400

    Add project files.

---
+++ Buttons/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Buttons;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button1_Click(object sender, RoutedEventArgs e)
+	{
+		((Button)sender).Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb
+				((byte)Random.Shared.Next(), (byte)Random.Shared.Next(), (byte)Random.Shared.Next()));
+
+		MessageBox.Show("Button 1 Color Changed", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+
+	private void Button2_Click(object sender, RoutedEventArgs e)
+	{
+		((Button)sender).Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb
+			((byte)Random.Shared.Next(), (byte)Random.Shared.Next(), (byte)Random.Shared.Next()));
+
+		MessageBox.Show("Button 2 Color Changed", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+
+	private void Button3_Click(object sender, RoutedEventArgs e)
+	{
+		((Button)sender).Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb
+				((byte)Random.Shared.Next(), (byte)Random.Shared.Next(), (byte)Random.Shared.Next()));
+
+		MessageBox.Show("Button 3 Color Changed", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+
+	private void Button4_Click(object sender, RoutedEventArgs e)
+	{
+		if (e is MouseButtonEventArgs mouse)
+		{
+				MessageBox.Show("Button 4 vis", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+			if (mouse.RightButton == MouseButtonState.Pressed)
+			{
+				((Button)sender).Visibility = Visibility.Hidden;
+				((Button)sender).IsEnabled = false;
+				return;
+			}
+		}
+
+		((Button)sender).Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb
+				((byte)Random.Shared.Next(), (byte)Random.Shared.Next(), (byte)Random.Shared.Next()));
+
+		MessageBox.Show("Button 4 Color Changed", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+
+	private void Button5_Click(object sender, RoutedEventArgs e)
+	{
+		((Button)sender).Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb
+				((byte)Random.Shared.Next(), (byte)Random.Shared.Next(), (byte)Random.Shared.Next()));
+
+		MessageBox.Show("Button 5 Color Changed", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+
+	private void Button6_Click(object sender, RoutedEventArgs e)
+	{
+		((Button)sender).Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb
+				((byte)Random.Shared.Next(), (byte)Random.Shared.Next(), (byte)Random.Shared.Next()));
+
+		MessageBox.Show("Button 6 Color Changed", "Color", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+}
